4d783cfc520c264bd2177dd5c5db06ff164416ad
jep <jp-github@blacktron.com>
2012-01-05 11:35:12 -0600

    Major update to have all the bells and whistles, plus docs

---
+++ docs/output/tree.js
@@ -0,0 +1,13 @@
+
+		Docs.classData ={"id":"apidocs","iconCls":"icon-docs","text":"API Documentation","singleClickExpand":true,"children":[
+                {"id":"pkg-jep","text":"jep","iconCls":"icon-pkg","cls":"package","singleClickExpand":true, children:[
+                {"id":"pkg-jep.field","text":"field","iconCls":"icon-pkg","cls":"package","singleClickExpand":true, children:[
+                {"href":"output/jep.field.Slider.html","text":"Slider","id":"jep.field.Slider","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				]}
+				]}
+				]};
+        Docs.icons = {
+        
+			"jep.field.Slider":"icon-cls"
+			};
+    
